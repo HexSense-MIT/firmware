477be753b64d764ec07304e4e9efd06d5088cab9
fangzheng liu <bitliufangzheng@gmail.com>
2025-01-09 13:52:34 -0500

    mac:added ESP32-S3 timer test

---
+++ ESP32-S3_timer_test/src/main.c
@@ -0,0 +1,9 @@
+#include "soc/rtc_cntl_reg.h"  // For RTC_CNTL_CLK_CONF_REG
+#include "soc/soc.h"           // For register bit definitions
+#include "esp_rom_sys.h"       // For esp_rom_delay_us
+
+
+void app_main()
+{
+  ;
+}
