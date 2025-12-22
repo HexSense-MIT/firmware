cf20a622dcbed30b143f6b780f2bbbe16dbb26a9
fangzheng liu <bitliufangzheng@gmail.com>
2025-12-21 22:15:47 -0500

    mac: added seis_ADS_SD, for HexSense seis read ADS data and store in microsd card

---
+++ STM32F722/ADS131M04/seis_ADS131M04_DMA_MicroSD/Core/Inc/microsd_fz.h
@@ -0,0 +1,34 @@
+/*
+ * microsd_fz.h
+ *
+ *  Created on: Jul 25, 2025
+ *      Author: liufangzheng
+ */
+
+#ifndef INC_MICROSD_FZ_H_
+#define INC_MICROSD_FZ_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "main.h"
+#include "fatfs.h"
+#include "stm32f7xx_hal.h"
+
+//some variables for FatFs
+extern FATFS FatFs;   //Fatfs handle
+extern FIL fil;       //File handle
+
+extern FRESULT mount_sd_card(void);
+extern FRESULT eject_sd_card(void);
+extern FRESULT get_statistics(DWORD *free_clusters, DWORD *free_sectors, DWORD *total_sectors);
+extern FRESULT open_file(const char *filename);
+extern FRESULT write_data_to_file(uint8_t *data, UINT data_len);
+extern FRESULT close_file(void);
+
+#endif /* INC_MICROSD_FZ_H_ */
